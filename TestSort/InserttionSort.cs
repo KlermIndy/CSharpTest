cdc36e97361d03210941161423f977367fe73cf8
Klerm Indy <teerayut_got@hotmail.com>
2017-08-21 10:30:31 +0700

    Add InserttionSort.cs

---
+++ TestSort/InserttionSort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSort
+{
+    class InserttionSort
+    {
+        public static int[] Sort(int[] tmplst)
+        {
+            int count = tmplst.Length;
+            for (int i = 0; i < count; i++)
+            {
+            }
+
+            return tmplst;
+        }
+    }
+}
