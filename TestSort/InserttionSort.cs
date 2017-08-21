f0fe82db253f75f432bfea5324cdae6b3b99211d
Klerm Indy <teerayut_got@hotmail.com>
2017-08-21 11:45:11 +0700

    Update InserttionSort.cs

--- TestSort/InserttionSort.cs
+++ TestSort/InserttionSort.cs
@@ -11,8 +11,21 @@ namespace TestSort
         public static int[] Sort(int[] tmplst)
         {
             int count = tmplst.Length;
-            for (int i = 0; i < count; i++)
+            for (int i = 1; i < count; i++)
             {
+                int insertindex = i;
+                for (int j = i -1; j >= 0; j--)
+                {
+                    if (tmplst[j] > tmplst[insertindex])
+                    {
+                        // Swap
+                        int tmp = tmplst[j];
+                        tmplst[j] = tmplst[insertindex];
+                        tmplst[insertindex] = tmp;
+
+                        insertindex = j;
+                    }
+                }
             }
 
             return tmplst;
